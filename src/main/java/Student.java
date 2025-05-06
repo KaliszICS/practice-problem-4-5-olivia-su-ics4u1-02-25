dc5e8794b2497f11c719cd5d23b7da13dd229653
olivia-su-ics4u1-02-25 <olivia.su@student.tdsb.on.ca>
2025-05-06 13:15:13 +0000

    commit

---
+++ src/main/java/Student.java
@@ -0,0 +1,41 @@
+class Student{
+    private String name;
+	private int age;
+    private String studentNumber;
+
+	public Student(String name, int age, String studentNumber) {
+		this.name = name;
+		this.age = age;
+        this.studentNumber = studentNumber;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+    public String getStudentNumber(){
+        return this.studentNumber;
+    }
+
+    @Override
+    public String toString(){
+        return this.name + ", " + this.age + " - " + this.studentNumber;
+    }
+
+    @Override
+	public boolean equals(Object obj) { 
+		if (obj == null || obj.getClass() != this.getClass()){
+			return false;
+		}
+		Student student = (Student)obj;
+		if (student.getStudentNumber().equals(this.studentNumber)) {
+			return true;
+		}
+		return false;
+	}
+
+}
